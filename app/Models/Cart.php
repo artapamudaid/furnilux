755d5be62528f6d78e51d0c1ee246dce278cd5b7
artapamudaid <pamuda.id@gmail.com>
2022-04-07 14:29:44 +0700

    add cart model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'users_id', 'products_id'
+    ];
+
+    public function products()
+    {
+        return $this->hasOne(Product::class, 'id', 'products_id');
+    }
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
 }
