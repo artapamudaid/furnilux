7521d9fadf83b80a51ce694c5ba632af0d2e3245
artapamudaid <pamuda.id@gmail.com>
2022-04-11 09:52:53 +0700

    create product galleries

---
+++ app/Http/Controllers/ProductGalleryController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductGalleryRequest;
+use App\Models\Product;
+use App\Models\ProductGallery;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Yajra\DataTables\Facades\DataTables;
+
+class ProductGalleryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Product $product)
+    {
+        if (request()->ajax()) {
+            $productGallery = ProductGallery::query();
+
+            return DataTables::of($productGallery)
+                ->addColumn('action', function ($item) {
+                    return '
+                    <form class="inline-block" action="' . route('dashboard.product.gallery.destroy', $item->id) . '" method="POST">
+                    <button class="inline-flex items-center h-6 bg-red-500 hover:bg-red-700 text-sm text-white font-bold py-2 px-2 rounded shadow-lg">
+                    ' . method_field('delete') . csrf_field() . '
+                    Delete
+                    </button>
+                    </form>
+                    ';
+                })
+                ->editColumn('url', function ($item) {
+                    return '<img style="max-width: 150pxW" src="' . Storage::url($item->url) . '"/>';
+                })
+                ->editColumn('is_featured', function ($item) {
+                    return $item->is_featured ? 'Yes' : 'No';
+                })
+                ->rawColumns(['action'])
+                ->make();
+        }
+
+        return view('pages.backend.galleries.index', compact('product'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Product $product)
+    {
+        return view('pages.backend.galleries.create', compact('product'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductGalleryRequest $request, Product $product)
+    {
+        $files = $request->file('files');
+
+        if ($request->hasFile('files')) {
+            foreach ($files as $file) {
+                $path = $file->store('public/galleries');
+
+                ProductGallery::create([
+                    'product_id' => $product->id,
+                    'url' => $path
+                ]);
+            }
+        }
+
+        return redirect()->route('dashboard.product.gallery.index', $product->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductGallery $gallery)
+    {
+        $gallery->delete();
+
+        return redirect()->route('dashboard.product.gallery.index', $gallery->product_id);
+    }
+}
