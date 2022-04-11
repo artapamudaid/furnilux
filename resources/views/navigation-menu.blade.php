d65a4d67883385a15174e2dbb047534b1bd778b6
artapamudaid <pamuda.id@gmail.com>
2022-04-11 10:00:04 +0700

    update navmenu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,15 +12,19 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('dashboard.index') }}" :active="request()->routeIs('dashboard')">
+                    <x-jet-nav-link href="{{ route('dashboard.index') }}"
+                        :active="request()->routeIs('dashboard.index')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+
+                    @if (Auth::user()->roles == 'ADMIN')
+
                     <x-jet-nav-link href="{{ route('dashboard.product.index') }}"
                         :active="request()->routeIs('dashboard.product.*')">
                         {{ __('Product') }}
                     </x-jet-nav-link>
+
+                    @endif
                 </div>
             </div>
 
@@ -156,9 +160,19 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-jet-responsive-nav-link href="{{ route('dashboard.index') }}" :active="request()->routeIs('dashboard')">
+            <x-jet-responsive-nav-link href="{{ route('dashboard.index') }}"
+                :active="request()->routeIs('dashboard.index')">
                 {{ __('Dashboard') }}
             </x-jet-responsive-nav-link>
+
+            @if (Auth::user()->roles == 'ADMIN')
+
+            <x-jet-responsive-nav-link href="{{ route('dashboard.product.index') }}"
+                :active="request()->routeIs('dashboard.product.*')">
+                {{ __('Product') }}
+            </x-jet-responsive-nav-link>
+
+            @endif
         </div>
 
         <!-- Responsive Settings Options -->
